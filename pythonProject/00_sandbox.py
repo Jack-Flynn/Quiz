125af9f7f1382ccf70f260af5fc017c42f743599
Jack-Flynn <flynn.jack@awatapu.school.nz>
2021-07-05 13:20:59 +1200

    Sandbox thingie with loops now!

--- pythonProject/00_sandbox.py
+++ pythonProject/00_sandbox.py
@@ -1,14 +1,14 @@
-#Ask users for a number
+# Ask users for a number
 get_number = int(input("Choose a number? "))
 
-#Multiply the number by 5
+# Multiply the number by 5
 times_five = get_number * 5
 
-#Output the result
+# Output the result
 print("{} times five is equal to {}".format(get_number, times_five))
 
 
-#get name until exit code is entered
+# Get name until exit code is entered
 
 name = ""
 while name.lower() != "xxx":

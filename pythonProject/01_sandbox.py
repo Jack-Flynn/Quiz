3121ef0a17bf742fec3bd1074ceb67a812a30f32
Jack-Flynn <flynn.jack@awatapu.school.nz>
2021-07-08 09:42:51 +1200

    i don't know what this is?

---
+++ pythonProject/01_sandbox.py
@@ -0,0 +1,8 @@
+while True:
+    def test():
+        from random import randint
+        # generate some integers
+        for _ in range(1):
+            numbers = randint(0, 100)
+        print(numbers)
+    test()

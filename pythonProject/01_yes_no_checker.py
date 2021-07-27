6b56062158229ab9e733897747752ea3811006cf
Jack-Flynn <flynn.jack@awatapu.school.nz>
2021-07-27 13:21:57 +1200

    Yes/No Checker

--- pythonProject/01_yes_no_checker.py
+++ pythonProject/01_yes_no_checker.py
@@ -15,9 +15,12 @@ def yes_no(question):
         else:
             print("Please answer yes / no")
 # Main routine goes here...
-show_instructions = yes_no("Have you played the game before")
-print("You chose {}".format(show_instructions))
-print()
-having_fun = yes_no("Are you having fun? ")
-print("You said {} to having fun".format(having_fun))
 
+show_instructions = yes_no("Have you played the game before")
+
+if show_instructions == "no" or show_instructions == "n":
+    print("Show instructions")
+
+elif show_instructions == "yes" or show_instructions == "y":
+    print("We will not show instructions")
+print()

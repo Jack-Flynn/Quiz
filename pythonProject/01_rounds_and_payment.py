c57f4add01f8e213d224aff7336fa5d305e82199
Jack-Flynn <flynn.jack@awatapu.school.nz>
2021-08-09 12:17:51 +1200

    Rounds and Payment that kinda works

--- pythonProject/01_rounds_and_payment.py
+++ pythonProject/01_rounds_and_payment.py
@@ -1,7 +1,20 @@
 # Ask for payment
 print("Costs: $1 per round, up to 10 rounds")
-payment = int(input("How much do you wish to pay?"))
-if payment >= 1 and payment <= 10:
-    print("Continue")
-else:
-    print("Please enter a whole number between 1 and 10")
+def num_check(question, low, high):
+    error = "Please enter a whole number between 1 and 10"
+
+    valid = False
+    while not valid:
+        try:
+            response = int(input(question))
+
+            if low < response  <= high:
+                return response
+
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+how_much = num_check("How much would you like to play with?", 0, 10)
+

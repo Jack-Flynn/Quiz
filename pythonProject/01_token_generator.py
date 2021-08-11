82e1b7ba9ea92461062d104fc771e96a2ff348ab
Jack-Flynn <flynn.jack@awatapu.school.nz>
2021-08-11 14:33:51 +1200

    Token Generator Better

--- pythonProject/01_token_generator.py
+++ pythonProject/01_token_generator.py
@@ -1,17 +1,19 @@
-# define function
+
+# Generate some tokens
 def token_generator():
+    global token
     from random import randint
     # generate some integers
     for _ in range(1):
         token = randint(1, 100)
     if token <= 25:
         print("Donkey")
-    elif token > 25 and token <= 50:
+    elif 25 < token <= 50:
         print("Horse")
-    elif token > 50 and token <= 75:
+    elif 50 < token <= 75:
         print("Zebra")
     else:
         print("Unicorn")
+
+
 token_generator()
-
-

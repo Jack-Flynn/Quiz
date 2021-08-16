572524b91df412e53e00d9191f8b58f5be289611
Jack-Flynn <flynn.jack@awatapu.school.nz>
2021-08-16 13:17:00 +1200

    Pretty much Main Code

--- pythonProject/rounds_and_statements.py
+++ pythonProject/rounds_and_statements.py
@@ -1,6 +1,4 @@
-# Greet the user
-print("Welcome to Lucky Unicorns!")
-
+# Functions go here
 def yes_no(question):
     valid = False
     while not valid:
@@ -13,21 +11,7 @@ def yes_no(question):
             return response
         else:
             print("Please answer yes / no")
-# Main routine goes here...
 
-show_instructions = yes_no("Have you played the game before?")
-
-if show_instructions == "no" or show_instructions == "n":
-    print("You will pay a amount of up to $10 (with a minimum of $1)")
-    print("You will then be given a token")
-    print("If your token is a donkey then, bad luck, you lose, but if it is a horse or a zebra then you win 50c")
-    print("If your token is a unicorn then you win $5")
-    print("OK! we are ready to continue now!")
-elif show_instructions == "yes" or show_instructions == "y":
-    print("OK! Moving on!")
-print()
-
-# Functions here
 def num_check(question, low, high):
     error = "Please enter a whole number between 1 and 10"
 
@@ -49,14 +33,29 @@ def token_generator():
     for _ in range(1):
         token = randint(1, 100)
     if token <= 50:
-        print("Donkey")
-    elif 50 < token <= 75:
-        print("Horse")
-    elif 75 < token <= 90:
-        print("Zebra")
+        print("Bad luck, you got a Donkey, you win nothing and lose nothing.")
+    elif 50 < token <= 80:
+        print("You got a Horse! You win 50 cents!")
+    elif 80 < token <= 95:
+        print("You got a Zebra! You win 50 cents!")
     else:
-        print("Unicorn")
+        print("What incredible luck! you got a Unicorn! You win 5 dollars!")
 
+
+# Greet the user
+print("Welcome to Lucky Unicorns!")
+
+show_instructions = yes_no("Have you played the game before?")
+
+if show_instructions == "no" or show_instructions == "n":
+    print("You will pay a amount of up to $10 (with a minimum of $1)")
+    print("You will then be given a token")
+    print("If your token is a donkey then, bad luck, you lose, but if it is a horse or a zebra then you win 50c")
+    print("If your token is a unicorn then you win $5")
+    print("OK! We are ready to continue now!")
+elif show_instructions == "yes" or show_instructions == "y":
+    print("OK! Moving on!")
+print()
 
 # Ask for payment
 how_much = num_check("How much would you like to play with?", 0, 10)
@@ -84,8 +83,12 @@ while play_again != "quit":
         how_much += 5
     print("Balance: ", how_much)
     print()
+
     if how_much <= 0:
-        print("you are broke! go home!")
+        print("You have ran out of money and cannot play any more rounds!")
         break
 
     play_again = input("Press <Enter> to play again or type 'quit' to exit").lower().strip()
+    if play_again == "quit":
+        print("Exiting code!")
+        break

97f3401833c4b5d87b1de53570ce007972ec61d7
Jack-Flynn <flynn.jack@awatapu.school.nz>
2021-09-22 15:08:10 +1200

    introductions and instructions now with colour!

--- Quiz/Introductions and Instructions.py
+++ Quiz/Introductions and Instructions.py
@@ -1,4 +1,5 @@
 # Functions go here
+
 def yes_no(question):
     valid = False
     while not valid:
@@ -15,9 +16,17 @@ def yes_no(question):
         else:
             print("Please answer yes / no")
 
+print("\033[1;32;40m")
+
 # Store all values for yes and no
 yes = ["yes", "y", "affirmative", "positive", "correct", "yep", "yeah"]
 no = ["no", "n", "wrong", "incorrect", "negative", "nah", "nope"]
 
-instructions = yes_no("Have you played")
-print(no_yes)
+# Ask user whether or not to show instructions
+print("Welcome to placeholderquizname!")
+instructions = yes_no("Have you played this quiz before?")
+
+if instructions in no:
+    print("any details yet jack?")
+elif instructions in yes:
+    print("righty-ho moving on")

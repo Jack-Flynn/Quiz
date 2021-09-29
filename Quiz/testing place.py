e16ca8770b64d7afe4f9a685e9ec54c0bbc4b249
Jack-Flynn <flynn.jack@awatapu.school.nz>
2021-09-30 09:57:52 +1300

    Functions not working properly

--- Quiz/testing place.py
+++ Quiz/testing place.py
@@ -1,25 +1,43 @@
-def question1 (question):
-        response = input(question).lower().strip()
+def questions (question):
+    strikes = 0
+    ticks = 0
+    questions_answered = 0
+    response = input(question)
 
-        if response == "a":
-            print("well done 'a' was correct")
-            ticks += 1
-            questions_answered += 1
-        else:
-            print("you are wrong")
-            strikes += 1
-            questions_answered += 1
-strikes = 0
-ticks = 0
-questions_answered = 0
-question_one = question1("Question 1: What is the correct spelling of the fear of long words (sorry if you're dyslexic)?\n"
+    if response == "a":
+        print("well done 'a' was correct")
+        ticks += 1
+        questions_answered += 1
+    else:
+        print("you are wrong")
+        strikes += 1
+        questions_answered += 1
+    if strikes == 3:
+        print("That's 3 strikes! You're out!")
+    elif questions_answered > 4:
+        print("well now we're done aren't we?")
+
+
+question_one = questions("Question 1: What is the correct spelling of the fear of long words (sorry if you're dyslexic)1?\n"
                       "A: hippopotomonstrosesquippedaliophobia\n"
                       "B: hippopotomonstrosesquippadeliophobia\n"
-                      "C: hippopotomonstrosesquipadeliophobia").strip().lower()
-if strikes == 3:
-    print("That's 3 strikes! You're out!")
-elif questions_answered > 4:
-    print("well now we're done aren't we?")
-print(strikes)
-print(ticks)
-print(questions_answered)
+                      "C: hippopotomonstrosesquipadeliophobia\n")
+question_two = questions("Question 1: What is the correct spelling of the fear of long words (sorry if you're dyslexic)2?\n"
+                      "A: hippopotomonstrosesquippedaliophobia\n"
+                      "B: hippopotomonstrosesquippadeliophobia\n"
+                      "C: hippopotomonstrosesquipadeliophobia\n")
+question_three = questions("Question 1: What is the correct spelling of the fear of long words (sorry if you're dyslexic)3?\n"
+                      "A: hippopotomonstrosesquippedaliophobia\n"
+                      "B: hippopotomonstrosesquippadeliophobia\n"
+                      "C: hippopotomonstrosesquipadeliophobia\n")
+question_four = questions("Question 1: What is the correct spelling of the fear of long words (sorry if you're dyslexic)4?\n"
+                      "A: hippopotomonstrosesquippedaliophobia\n"
+                      "B: hippopotomonstrosesquippadeliophobia\n"
+                      "C: hippopotomonstrosesquipadeliophobia\n")
+question_five = questions("Question 1: What is the correct spelling of the fear of long words (sorry if you're dyslexic)5?\n"
+                      "A: hippopotomonstrosesquippedaliophobia\n"
+                      "B: hippopotomonstrosesquippadeliophobia\n"
+                      "C: hippopotomonstrosesquipadeliophobia\n")
+print(questions.strikes)
+print(questions.ticks)
+print(questions.questions_answered)

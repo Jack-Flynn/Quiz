31ec0552879acb528d7ac0dfcebcadf050a41041
Jack-Flynn <flynn.jack@awatapu.school.nz>
2021-11-02 13:27:26 +1300

    Testing things with actual progress

--- Quiz/testing place.py
+++ Quiz/testing place.py
@@ -1,28 +1,34 @@
-def questions (question):
-    response = input(question).lower().strip()
-    if response == "a":
-        print("well done 'a' was correct")
-    else:
-        print("you are wrong (sarcastic comment here)")
+def questions (question, answer):
+    global ticks
+    global strikes
+    global questions_answered
+    error = "please enter a number"
+
+    try:
+        response = int(input(question))
+        if response == answer:
+            print("Correct")
+            ticks += 1
+            questions_answered += 1
+            return response
+        else:
+            print("incorrect")
+            strikes += 1
+            questions_answered += 1
+    except ValueError:
+        print(error)
 
 
-questions("Question 1:\n"
-          "A: a\n"
-          "B: b\n"
-          "C: c\n")
-questions("Question 2:\n"
-          "A: a\n"
-          "B: b\n"
-          "C: c\n")
-questions("Question 3:\n"
-          "A: a\n"
-          "B: b\n"
-          "C: c\n")
-questions("Question 4:\n"
-          "A: a\n"
-          "B: b\n"
-          "C: c\n")
-questions("Question 5:\n"
-          "A: a\n"
-          "B: b\n"
-          "C: c\n")
+ticks = 0
+strikes = 0
+questions_answered = 0
+while strikes < 3 and questions_answered != 5:
+    question1 = questions("Question 1:\n""1: a\n""2: b\n""3: c", 1)
+    question2 = questions("Question 2:\n""1: a\n""2: b\n""3: c", 1)
+    question3 = questions("Question 3:\n""1: a\n""2: b\n""3: c", 1)
+    question4 = questions("Question 4:\n""1: a\n""2: b\n""3: c", 1)
+    question5 = questions("Question 5:\n""1: a\n""2: b\n""3: c", 1)
+
+print(ticks)
+print(strikes)
+print(questions_answered)
